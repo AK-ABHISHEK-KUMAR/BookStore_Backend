49e8ba9bfa02e5664256958dd4404ef05ede2798
Abhishek Kumar <ak906626@gmail.com>
2022-05-21 13:32:00 +0530

    Initial Commit Server

--- index.js
+++ index.js
@@ -10,45 +10,6 @@ let db = null;
 app.use(express.json());
 app.use(cors());
 
-// const list = [
-//     {
-//         id: 1,
-//         title: "Harry Potter and the Philosopher's Stone",
-//         price: 10
-//     },
-//     {
-//         id: 2,
-//         title: "Harry Potter and the Chamber of Secrets",
-//         price: 15
-//     },
-//     {
-//         id: 3,
-//         title: "Harry Potter and the Prisoner of Azkaban",
-//         price: 20
-//     },
-//     {
-//         id: 4,
-//         title: "Harry Potter and the Goblet of Fire",
-//         price: 50
-//     },
-//     {
-//         id: 5,
-//         title: "Harry Potter and the Order of the Phoenix",
-//         price: 10
-//     },
-//     {
-//         id: 6,
-//         title: "Harry Potter and the Half-Blood Prince",
-//         price: 30
-//     },
-//     {
-//         id: 7,
-//         title: "Harry Potter and the Deathly Hallows",
-//         price: 40
-//     }
-// ]
-
-// let x = 0;
 
 app.post('/users', async (req, res) => {
 
